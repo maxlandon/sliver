b48a9023cdaeaa52ebdf5f96bb378f4e43ccda14
maxlandon <maximelandon@gmail.com>
2021-09-15 20:46:17 -0400

    Updated canaries table

--- client/command/server/canaries/canaries.go
+++ client/command/server/canaries/canaries.go
@@ -19,16 +19,14 @@ package canaries
 */
 
 import (
-	"bytes"
 	"context"
-	"fmt"
-	"strings"
-	"text/tabwriter"
 
 	"github.com/bishopfox/sliver/client/log"
 	"github.com/bishopfox/sliver/client/transport"
+	"github.com/bishopfox/sliver/client/util"
 	"github.com/bishopfox/sliver/protobuf/clientpb"
 	"github.com/bishopfox/sliver/protobuf/commonpb"
+	"github.com/maxlandon/readline"
 )
 
 const (
@@ -61,48 +59,75 @@ func (c *Canaries) Execute(args []string) (err error) {
 
 func displayCanaries(canaries []*clientpb.DNSCanary, burnedOnly bool) {
 
-	outputBuf := bytes.NewBufferString("")
-	table := tabwriter.NewWriter(outputBuf, 0, 2, 2, ' ', 0)
+	table := util.NewTable("")
 
-	fmt.Fprintf(table, "Sliver Name\tDomain\tTriggered\tFirst Trigger\tLatest Trigger\t\n")
-	fmt.Fprintf(table, "%s\t%s\t%s\t%s\t%s\t\n",
-		strings.Repeat("=", len("Sliver Name")),
-		strings.Repeat("=", len("Domain")),
-		strings.Repeat("=", len("Triggered")),
-		strings.Repeat("=", len("First Trigger")),
-		strings.Repeat("=", len("Latest Trigger")),
-	)
+	headers := []string{"Name", "Domain", "Triggered", "First Trigger", "Latest Trigger"}
+	headLen := []int{0, 20, 0, 20, 20}
+	table.SetColumns(headers, headLen)
 
-	lineColors := []string{}
 	for _, canary := range canaries {
 		if burnedOnly && !canary.Triggered {
 			continue
 		}
-		fmt.Fprintf(table, "%s\t%s\t%s\t%s\t%s\t\n",
-			canary.ImplantName,
-			canary.Domain,
-			fmt.Sprintf("%v", canary.Triggered),
-			canary.FirstTriggered,
-			canary.LatestTrigger,
-		)
-		if canary.Triggered {
-			lineColors = append(lineColors, bold+red)
-		} else {
-			lineColors = append(lineColors, normal)
-		}
-	}
-	table.Flush()
 
-	for index, line := range strings.Split(outputBuf.String(), "\n") {
-		if len(line) == 0 {
-			continue
-		}
-		// We need to account for the two rows of column headers
-		if 0 < len(line) && 2 <= index {
-			lineColor := lineColors[index-2]
-			fmt.Printf("%s%s%s\n", lineColor, line, normal)
+		var triggered string
+		if canary.Triggered {
+			triggered = readline.Red("Yes")
 		} else {
-			fmt.Printf("%s\n", line)
+			triggered = readline.Green("No")
 		}
+
+		table.AppendRow([]string{canary.ImplantName, canary.Domain, triggered, canary.FirstTriggered, canary.LatestTrigger})
 	}
+
+	// Print table
+	table.Output()
 }
+
+// func displayCanaries(canaries []*clientpb.DNSCanary, burnedOnly bool) {
+//
+//         outputBuf := bytes.NewBufferString("")
+//         table := tabwriter.NewWriter(outputBuf, 0, 2, 2, ' ', 0)
+//
+//         fmt.Fprintf(table, "Sliver Name\tDomain\tTriggered\tFirst Trigger\tLatest Trigger\t\n")
+//         fmt.Fprintf(table, "%s\t%s\t%s\t%s\t%s\t\n",
+//                 strings.Repeat("=", len("Sliver Name")),
+//                 strings.Repeat("=", len("Domain")),
+//                 strings.Repeat("=", len("Triggered")),
+//                 strings.Repeat("=", len("First Trigger")),
+//                 strings.Repeat("=", len("Latest Trigger")),
+//         )
+//
+//         lineColors := []string{}
+//         for _, canary := range canaries {
+//                 if burnedOnly && !canary.Triggered {
+//                         continue
+//                 }
+//                 fmt.Fprintf(table, "%s\t%s\t%s\t%s\t%s\t\n",
+//                         canary.ImplantName,
+//                         canary.Domain,
+//                         fmt.Sprintf("%v", canary.Triggered),
+//                         canary.FirstTriggered,
+//                         canary.LatestTrigger,
+//                 )
+//                 if canary.Triggered {
+//                         lineColors = append(lineColors, bold+red)
+//                 } else {
+//                         lineColors = append(lineColors, normal)
+//                 }
+//         }
+//         table.Flush()
+//
+//         for index, line := range strings.Split(outputBuf.String(), "\n") {
+//                 if len(line) == 0 {
+//                         continue
+//                 }
+//                 // We need to account for the two rows of column headers
+//                 if 0 < len(line) && 2 <= index {
+//                         lineColor := lineColors[index-2]
+//                         fmt.Printf("%s%s%s\n", lineColor, line, normal)
+//                 } else {
+//                         fmt.Printf("%s\n", line)
+//                 }
+//         }
+// }
